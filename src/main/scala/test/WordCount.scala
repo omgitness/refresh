9d0da6058ea85e7658a6b6de76f0456bd0b8496b
nadalim <nadalim@dxc.com>
2019-06-25 14:59:04 +0800

    added .gitignore; word count working

---
+++ src/main/scala/test/WordCount.scala
@@ -0,0 +1,25 @@
+package test
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+  final def main(args: Array[String]) {
+    val sparkConf = new SparkConf
+    sparkConf.setAppName("WordCount").setMaster("local[*]")
+    val sc = new SparkContext(sparkConf)
+    
+    //val input = List("the","big","bad","wolf","jumped","over","the","lazy","fox")
+    //val rdd = sc.parallelize(input)
+
+    val rdd = sc.textFile("hdfs://localhost:9000/test/input/sample.txt")
+    rdd.mapPartitions(_.flatMap(x => {
+      val words = x.toLowerCase.split(" ", -1)
+      words.flatMap(y => {
+        if(y.isEmpty) None
+        else Some(y, 1)
+      })
+    }))
+      .reduceByKey((x, y) => x + y)
+      .saveAsTextFile("hdfs://localhost:9000/test/output")
+  }
+}
