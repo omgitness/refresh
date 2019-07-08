59c6ed70b6eb6cee0392a8b10fcaca6f7ec2c6b8
root <root@localhost.localdomain>
2019-07-08 11:41:19 +0800

    updated WordCount classes

--- src/main/java/test/mapreduce/WordCount.java
+++ src/main/java/test/mapreduce/WordCount.java
@@ -1,11 +1,11 @@
 package test.mapreduce;
 
-import java.lang.InterruptedException;
 import java.io.IOException;
-import java.util.Iterator;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -20,8 +20,8 @@ public class WordCount {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
-    public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-      String words[] = value.toString().split(" ", -1);
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+      String words[] = value.toString().toLowerCase().split(" ", -1);
       for(int i = 0; i < words.length; i++) {
         word.set(words[i]);
         context.write(word, one);
@@ -30,25 +30,27 @@ public class WordCount {
   }
 
   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-    public void reduce(Text key, Iterator<IntWritable> values, Reducer.Context context) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
       int sum = 0;
-      while (values.hasNext()) {
-        sum += values.next().get();
+      for(IntWritable val : values) {
+        sum += val.get();
       }
       context.write(key, new IntWritable(sum));
     }
   }
 
   public static void main(String[] args) throws Exception {
-    Job job = new Job();
+	Job job = new Job();
 
-    job.setJarByClass(WordCount.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-
+	job.setJobName("WordCount");
+	job.setJarByClass(WordCount.class);
     job.setMapperClass(Map.class);
-    //job.setCombinerClass(Reduce.class);
     job.setReducerClass(Reduce.class);
+
+	job.setMapOutputKeyClass(Text.class);
+	job.setMapOutputValueClass(IntWritable.class);
+	job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
 
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
--- src/main/scala/test/spark/WordCount.scala
+++ src/main/scala/test/spark/WordCount.scala
@@ -5,13 +5,19 @@ import org.apache.spark.{SparkConf, SparkContext}
 object WordCount {
   final def main(args: Array[String]) {
     val sparkConf = new SparkConf
-    sparkConf.setAppName("WordCount").setMaster("local[*]")
+    sparkConf.setAppName("WordCount")
+
+	if(sparkConf.getBoolean("spark.set.master", true))
+	  sparkConf.setMaster("local[*]")
+
     val sc = new SparkContext(sparkConf)
 
     //val input = List("the","big","bad","wolf","jumped","over","the","lazy","fox")
     //val rdd = sc.parallelize(input)
 
-    val rdd = sc.textFile("hdfs://localhost:9000/test/input/sample.txt")
+	val input = args(0)
+	val output = args(1)
+	val rdd = sc.textFile(input)
     rdd.mapPartitions(_.flatMap(x => {
       val words = x.toLowerCase.split(" ", -1)
       words.flatMap(y => {
@@ -20,6 +26,6 @@ object WordCount {
       })
     }))
       .reduceByKey((x, y) => x + y)
-      .saveAsTextFile("hdfs://localhost:9000/test/output")
+      .saveAsTextFile(output)
   }
 }
